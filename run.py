a1a46c2897fd9b36e4cbafe9afac604fc643c302
Jay Turner <jay@trainerdex.app>
2020-11-10 14:36:08 +0000

    chore: add git stuff

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from os import environ
 import traceback
 
+
 class Bot(commands.Bot):
     async def on_command_error(self, ctx, error):
         if isinstance(error, commands.MissingRequiredArgument):
@@ -10,9 +11,10 @@ class Bot(commands.Bot):
         elif isinstance(error, commands.CommandError):
             await ctx.send(error)
         else:
-            await ctx.send('Error executing {}, see logs for more details'.format(ctx.command))
+            await ctx.send("Error executing {}, see logs for more details".format(ctx.command))
             traceback.print_tb(error.__traceback__)
 
-bot = Bot(command_prefix='!', description='Monord')
+
+bot = Bot(command_prefix="!", description="Monord")
 bot.add_cog(Monord(bot))
-bot.run(environ['DISCORD_TOKEN'], bot=True, reconnect=True)
+bot.run(environ["DISCORD_TOKEN"], bot=True, reconnect=True)
